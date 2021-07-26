0657ce99293f4987a5e70bbe5a5e2b25af417e2c
phmdacosta <ph.mcosta@hotmail.com>
2021-07-26 17:44:57 +0100

    Correções

--- Ficha Base/scripts/background.lua
+++ Ficha Base/scripts/background.lua
@@ -2,11 +2,11 @@ Background={}
 
 function Background.export(sheet)
     local txt = Text:new()
+    txt:appendLine('----- Background -----')
 
     if sheet.backgroundTxt ~= nil 
         and sheet.backgroundTxt.p ~= nil 
         and sheet.backgroundTxt.p.e ~= nil then
-            txt:appendLine('----- Background -----')
             txt:breakLine()
             txt:appendLine(sheet.backgroundTxt.p.e.text)
     end
